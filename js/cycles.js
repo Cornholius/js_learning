b765fba86578174f00f16909093197a5e31a4d3e
lyi <y.layshkin@gmail.com>
2021-03-25 17:42:38 +0300

    циклы

---
+++ js/cycles.js
@@ -0,0 +1,22 @@
+// циклы
+
+let num = 50;
+
+// while (num < 60) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;   
+// }
+// while (num < 60);
+
+for (let i = 1; i < 100; i++) {
+    if (i == 7) {
+        break;
+    }
+    console.log(num);
+    num++;
+}
